81bc73415b82198d29d8a04190c54fa836cbc8a7
Marc Scheib <marc.scheib@gmx.de>
2016-03-25 15:34:55 +0100

    chore(*): basic app setup

---
+++ src/main.js
@@ -0,0 +1,14 @@
+import {ViewLocator, LogManager} from 'aurelia-framework';
+import {ConsoleAppender} from 'aurelia-logging-console';
+
+LogManager.addAppender(new ConsoleAppender());
+LogManager.setLevel(LogManager.logLevel.debug);
+
+export function configure(aurelia) {
+  aurelia.use
+    .defaultBindingLanguage()
+    .defaultResources();
+
+  aurelia.start()
+    .then(a => a.setRoot('app', document.body));
+}
