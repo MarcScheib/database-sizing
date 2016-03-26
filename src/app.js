63fde39fb8dfbf860b0af50a483d7526a544416e
Marc Scheib <marc.scheib@gmx.de>
2016-03-26 18:47:59 +0100

    feat(sizing): add day/week/year calculation result

--- src/app.js
+++ src/app.js
@@ -2,9 +2,35 @@ import {measurements} from './measurements';
 
 export class App {
   measurements = {};
+  day = 86400;
+  week = 604800;
+  year = 31536000;
 
   constructor() {
     this.measurements = measurements;
-    console.log(measurements);
+  }
+
+  get sizePerDay() {
+    let size = 0;
+    for (let i = 0; i < this.measurements.length; i++) {
+      size += this.measurements[i].size * this.measurements[i].number * this.day / this.measurements[i].interval;
+    }
+    return size;
+  }
+
+  get sizePerWeek() {
+    let size = 0;
+    for (let i = 0; i < this.measurements.length; i++) {
+      size += this.measurements[i].size * this.measurements[i].number * this.week / this.measurements[i].interval;
+    }
+    return size;
+  }
+
+  get sizePerYear() {
+    let size = 0;
+    for (let i = 0; i < this.measurements.length; i++) {
+      size += this.measurements[i].size * this.measurements[i].number * this.year / this.measurements[i].interval;
+    }
+    return size;
   }
 }
