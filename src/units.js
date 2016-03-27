81081aba0fb1cabfdfcc2e60c1a947e1f9c3efcc
Marc Scheib <marc.scheib@gmx.de>
2016-03-27 15:23:52 +0200

    feat(units): add units for sizing

---
+++ src/units.js
@@ -0,0 +1,32 @@
+export let units = [
+  {
+    id: 0,
+    string: 'Byte',
+    abbr: 'B',
+    factor: 1
+  },
+  {
+    id: 1,
+    string: 'Kilobyte',
+    abbr: 'kB',
+    factor: 1000
+  },
+  {
+    id: 2,
+    string: 'Megabyte',
+    abbr: 'MB',
+    factor: 1000 * 1000
+  },
+  {
+    id: 3,
+    string: 'Gigabyte',
+    abbr: 'GB',
+    factor: 1000 * 1000 * 1000
+  },
+  {
+    id: 4,
+    string: 'Terabyte',
+    abbr: 'TB',
+    factor: 1000 * 1000 * 1000 * 1000
+  }
+];
