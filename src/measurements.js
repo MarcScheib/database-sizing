4f97a512b732c9231da2a8018807203d5b9ecf9d
Marc Scheib <marc.scheib@gmx.de>
2016-04-02 12:51:58 +0200

    feat(sizing): add metadata for size of a measurement type

--- src/measurements.js
+++ src/measurements.js
@@ -1,12 +1,16 @@
 export let measurements = [
   {
     name: 'Ping',
+    values: 1,
+    sizePerValue: 48,
     size: 48,
     number: 0,
     interval: 60
   },
   {
     name: 'SNMP Interface',
+    values: 12,
+    sizePerValue: 64,
     size: 12 * 64,
     number: 0,
     interval: 300
